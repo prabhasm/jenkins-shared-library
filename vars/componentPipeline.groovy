1595b24bd4ba648c95afd0a12e37788ef5339530
prabhasm <prabhas.id@live.com>
2024-03-11 13:02:05 +0530

    initial

--- vars/componentPipeline.groovy
+++ vars/componentPipeline.groovy
@@ -1,19 +1,11 @@
+#!/usr/bin/env groovy
 
-
-def call(Map config, Closure body) {
+def call(Map config = [:], Closure body) {
     def debug = env.DEBUG
-    if (debug != null) {
-        config.debug = debug
-    }
+    if (debug != null) {config.debug = debug}
 
-    //def logger = new Logger(this, config.debug)
-    //def pipeline = new Pipeline(this, logger)
-    String processId = "${env.JOB_NAME}/${env.BUILD_NUMBER}"
-    try {
-        //pipeline.execute(config, body)
-    } finally {
 
-    }
+
 }
 
 return this
